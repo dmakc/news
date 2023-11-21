2b93d3a6b88c55e030359012941428d6aa900a97
Максим Давыдов <m.davidov92@gmail.com>
2023-11-21 13:32:54 +0300

    Add test_logic and test_content

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -2,11 +2,17 @@
 from datetime import datetime, timedelta
 
 from django.conf import settings
+# Импортируем функцию для получения модели пользователя.
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 # Импортируем функцию reverse(), она понадобится для получения адреса страницы.
 from django.urls import reverse
+from django.utils import timezone
 
-from news.models import News
+# Дополнительно к News импортируем модель комментария.
+from news.models import Comment, News
+
+User = get_user_model()
 
 # Текущая дата.
 today = datetime.today()
@@ -44,6 +50,53 @@ class TestHomePage(TestCase):
         sorted_dates = sorted(all_dates, reverse=True)
         # Проверяем, что исходный список был отсортирован правильно.
         self.assertEqual(all_dates, sorted_dates)
+
+
+class TestDetailPage(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(
+            title='Тестовая новость', text='Просто текст.'
+        )
+        # Сохраняем в переменную адрес страницы с новостью:
+        cls.detail_url = reverse('news:detail', args=(cls.news.id,))
+        cls.author = User.objects.create(username='Комментатор')
+        # Получите текущее время при помощи утилиты timezone.
+        now = timezone.now()
+        # Создаём комментарии в цикле.
+        for index in range(2):
+            # Создаём объект и записываем его в переменную.
+            comment = Comment.objects.create(
+                news=cls.news, author=cls.author, text=f'Tекст {index}',
+            )
+            # Сразу после создания меняем время создания комментария.
+            comment.created = now + timedelta(days=index)
+            # И сохраняем эти изменения.
+            comment.save()
+
+    def test_comments_order(self):
+        response = self.client.get(self.detail_url)
+        # Проверяем, что объект новости находится в словаре контекста
+        # под ожидаемым именем - названием модели.
+        self.assertIn('news', response.context)
+        # Получаем объект новости.
+        news = response.context['news']
+        # Получаем все комментарии к новости.
+        all_comments = news.comment_set.all()
+        # Проверяем, что время создания первого комментария в списке
+        # меньше, чем время создания второго.
+        self.assertLess(all_comments[0].created, all_comments[1].created)
+
+    def test_anonymous_client_has_no_form(self):
+        response = self.client.get(self.detail_url)
+        self.assertNotIn('form', response.context)
+
+    def test_authorized_client_has_form(self):
+        # Авторизуем клиент при помощи ранее созданного пользователя.
+        self.client.force_login(self.author)
+        response = self.client.get(self.detail_url)
+        self.assertIn('form', response.context)
 
 
 class TestContent(TestCase):
